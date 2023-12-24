d74a15951a3c077be7d429590bdffeb0fcd2b192
walaarehawe <129075533+walaarehawe@users.noreply.github.com>
2023-12-24 09:03:41 +0200

    w

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         state.print_grid();
      while (true){
          System.out.println("");
-         System.out.println("1ston1"+"   "+state.getplayer1().getStone1().getPosition());
-         System.out.println("1ston2"+"   "+state.getplayer1().getStone2().getPosition());
-         System.out.println("1ston3"+"   "+state.getplayer1().getStone3().getPosition());
-         System.out.println("1ston4"+"   "+state.getplayer1().getStone4().getPosition());
-         System.out.println("2ston1"+"   "+state.getplayer2().getStone1().getPosition());
-         System.out.println("2ston2"+"   "+state.getplayer2().getStone2().getPosition());
-         System.out.println("2ston3"+"   "+state.getplayer2().getStone3().getPosition());
-         System.out.println("2ston4"+"   "+state.getplayer2().getStone4().getPosition());
+//         System.out.println("1ston1"+"   "+state.getplayer1().getStone1().getPosition());
+//         System.out.println("1ston2"+"   "+state.getplayer1().getStone2().getPosition());
+//         System.out.println("1ston3"+"   "+state.getplayer1().getStone3().getPosition());
+//         System.out.println("1ston4"+"   "+state.getplayer1().getStone4().getPosition());
+//         System.out.println("2ston1"+"   "+state.getplayer2().getStone1().getPosition());
+//         System.out.println("2ston2"+"   "+state.getplayer2().getStone2().getPosition());
+//         System.out.println("2ston3"+"   "+state.getplayer2().getStone3().getPosition());
+//         System.out.println("2ston4"+"   "+state.getplayer2().getStone4().getPosition());
          State new_state=  state.play(state ,state.getplayer1());
          new_state.print_grid();
         new_state=  state.play(state ,state.getplayer2());
