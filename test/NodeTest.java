c553f19edba35737252ba47195d51c7800bae8a7
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2024-01-08 00:17:39 +0300

    adding basic test

--- test/NodeTest.java
+++ test/NodeTest.java
@@ -1,5 +1,16 @@
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 
 public class NodeTest {
+    @Test
+    public void testHeuristicReturnsMaxValue() {
+        State s = State.first_state();
+        assertFalse(s.isfinished(1));
+        System.out.println("hi");
+        // Node n = new Node("computer", null, s, 0);
 
+        // assertEquals(Integer.MAX_VALUE, n.heuristic());
+    }
 }
