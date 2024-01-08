a49950f5d39539997a41e111b5b140cb9d97ea00
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2024-01-08 21:16:52 +0300

    adding is finish test

--- test/NodeTest.java
+++ test/NodeTest.java
@@ -8,11 +8,18 @@ public class NodeTest {
         State s = State.first_state();
         Node n = new Node("computer", null, s, 0);
         assertFalse(n.isFinish());
+
         n.getState().getplayer1().getStone1().setPosition(84);
         n.getState().getplayer1().getStone2().setPosition(84);
         n.getState().getplayer1().getStone3().setPosition(84);
         n.getState().getplayer1().getStone4().setPosition(84);
-        System.out.println(n.getState().getplayer1().getStone1().getPosition());
+        assertTrue(n.isFinish());
+
+        n.getState().getplayer1().getStone1().setPosition(83);
+        n.getState().getplayer2().getStone1().setPosition(84);
+        n.getState().getplayer2().getStone2().setPosition(84);
+        n.getState().getplayer2().getStone3().setPosition(84);
+        n.getState().getplayer2().getStone4().setPosition(84);
         assertTrue(n.isFinish());
     }
 
