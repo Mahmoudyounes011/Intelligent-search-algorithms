1c982255728ba5960a69cae62d3101060581781c
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2024-01-08 21:14:52 +0300

    adding is finish test

--- test/NodeTest.java
+++ test/NodeTest.java
@@ -1,16 +1,25 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-
 public class NodeTest {
+    @Test
+    public void isFinish() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        State s = State.first_state();
+        Node n = new Node("computer", null, s, 0);
+        assertFalse(n.isFinish());
+        n.getState().getplayer1().getStone1().setPosition(84);
+        n.getState().getplayer1().getStone2().setPosition(84);
+        n.getState().getplayer1().getStone3().setPosition(84);
+        n.getState().getplayer1().getStone4().setPosition(84);
+        System.out.println(n.getState().getplayer1().getStone1().getPosition());
+        assertTrue(n.isFinish());
+    }
+
     @Test
     public void testHeuristicReturnsMaxValue() {
         State s = State.first_state();
-        assertFalse(s.isfinished(1));
-        System.out.println("hi");
-        // Node n = new Node("computer", null, s, 0);
-
+        Node n = new Node("computer", null, s, 0);
         // assertEquals(Integer.MAX_VALUE, n.heuristic());
     }
 }
