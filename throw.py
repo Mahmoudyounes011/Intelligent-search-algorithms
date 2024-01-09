787d8cd548e58edc9ac2f290d82a5e5b236b7643
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2024-01-09 03:05:55 +0300

    adding the property cut

--- throw.py
+++ throw.py
@@ -44,25 +44,37 @@
 
 
 
-def generate_throws(current_throw, current_sequence, all_sequences):
-    stones = ['dist', 'bnj', 'sheqah', 'bara', 'dowaq', 'three', 'four']
-    if current_throw == 10:
-        all_sequences.append(current_sequence)
+stones = ['dist', 'bnj', 'sheqah', 'bara', 'dowaq', 'three', 'four']
+stone_probabilities = {
+    'dist': 0.186624,  
+    'bnj': 0.0368864,  
+    'sheqah': 0.046656,
+    'bara': 0.004096,
+    'dowaq': 0.31104,
+    'three': 0.27648,
+    'four': 0.13824
+}
+def generate_throws(current_throw, current_sequence, all_sequences,cumulative_prob):
+    if current_throw == 3:
+        if cumulative_prob > 0.01:
+            all_sequences.append((current_sequence,cumulative_prob))
         return
     for stone in stones:
-        if stone in ['dist', 'bnj', 'sheqah', 'bara']:
-            generate_throws(current_throw + 1, current_sequence + [stone], all_sequences)
-        else:
-            all_sequences.append(current_sequence+[stone]) 
+        next_prob = stone_probabilities[stone]
+        if cumulative_prob * next_prob > 0.01:
+            if stone in ['dist', 'bnj', 'sheqah', 'bara']:
+                generate_throws(current_throw + 1, current_sequence + [stone], all_sequences,cumulative_prob * next_prob)
+            else:
+                all_sequences.append((current_sequence+[stone],cumulative_prob*stone_probabilities[stone])) 
     return         
 
 # Initial call to the function
 all_sequences = []
-generate_throws(0, [], all_sequences)
+generate_throws(0, [], all_sequences,1)
 
 # Print all possible sequences
-# for sequence in all_sequences:
-#     print(sequence)
+for sequence,prob in all_sequences:
+    print(f"Sequence: {sequence}, Probability: {prob:.2%}")
 
 
-print(len(all_sequences))
+print(f"Number of sequences with a probability > 1%: {len(all_sequences)}")
