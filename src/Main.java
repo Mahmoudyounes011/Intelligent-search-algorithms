dcfbb68a3ffaca5782a822b8b9f40033f6a7227a
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2024-01-12 19:22:46 +0300

    adding some test to the main

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,20 @@ import java.util.*;
 
 public class Main {
         public static void main(String[] args) {
-                State state = State.first_state();
-                // Grid.print_grid(state);
+                Algorithm algorithm = new Algorithm();
+                State s = State.first_state();
+                Node n = new Node("max", null, s, 1);
                 ArrayList<String> dices = new ArrayList<>();
-                ArrayList<State> movable = new ArrayList<>();
                 dices.add(Dice.KHAL);
-                dices.add(Dice.KHAL);
-                dices.add(Dice.DIST);
-                movable = State.nextstate(state, dices);
-                for (State m : movable) {
-                        // Grid.print_grid(m);
-                        System.out.println("#".repeat(20));
-                        System.out.println("player 2 stone 1" + " " +
-                                        m.getplayer2().getStone1().getPosition());
-                        System.out.println("player 2 stone 2" + " " +
-                                        m.getplayer2().getStone2().getPosition());
-                        System.out.println("player 2 stone 3" + " " +
-                                        m.getplayer2().getStone3().getPosition());
-                        System.out.println("player 2 stone 4" + " " +
-                                        m.getplayer2().getStone4().getPosition());
-
-                }
+                dices.add(Dice.BNJ);
+                // System.out.println("the possible state");
+                // ArrayList<State> states = State.nextstate(s, dices, 2);
+                // for (State state : states) {
+                // Grid.print_grid(state);
+                // }
+                n.setThrownDice(dices);
+                Node value = algorithm.expectIMinMax(n, 1, dices);
+                // System.out.println(value.getState());
+                // Grid.print_grid(value.getState());
         }
 }
